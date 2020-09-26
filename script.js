04d06d7016aff686696d9e837a5a439dcb1f00a7
Will Canino <willcaninoyt@gmail.com>
2020-09-26 23:36:17 +1000

    Finished All code, now working on solo projects to put in the (projects)

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let submitButton = document.querySelector('#submit-button');
+function emailValidate(email) {
+    if(email.includes('@')) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 
 function clickListener(event) {
     event.preventDefault();
-    console.log('Button clicked');
+    let emailInput = document.querySelector('#email');
+    let messageInput = document.querySelector('#message');
+
+    let emailText = emailInput.value;
+    let messageText = messageInput.value;
+
+    if(emailValidate(emailText) !== true) {
+    console.log('The email address must contain @');
+    return false;
+  }
+  console.log('Thanks for your message');
 }
+
+let submitButton = document.querySelector('#submit-button');
 
 submitButton.addEventListener('click', clickListener);
